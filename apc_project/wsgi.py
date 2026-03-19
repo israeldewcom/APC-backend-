b97253865f66655213b4f7e74bda5e31498ce6b6
israeldewcom <haddockthor@gmail.com>
2026-03-19 14:58:48 +0100

    Create wsgi.py

---
+++ apc_project/wsgi.py
@@ -0,0 +1,9 @@
+"""
+WSGI config for apc_project.
+"""
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apc_project.settings")
+
+application = get_wsgi_application()
